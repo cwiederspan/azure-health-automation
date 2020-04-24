a205f5993e6eea6039dca96823139aa23fb5396e
Chris Wiederspan <chris@wiederspan.com>
2020-04-24 23:57:38 +0000

    Updated to remove some of the v1 options

--- AsAzureFunctions/Functions.cs
+++ AsAzureFunctions/Functions.cs
@@ -1,26 +1,35 @@
 using System;
 using System.IO;
 using System.Net;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Mvc;
 
-using Newtonsoft.Json;
+using Microsoft.Azure.Management.FrontDoor;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Rest;
 
 namespace AsAzureFunctions {
 
     public static class MyFunctions {
 
-        private static HttpClient Client = new HttpClient();
-
-        private static string FILENAME = "/mnt/data/availability/error";
+        // Read in the settings from the Environment
+        private static string SSAS_DB_NAME = Environment.GetEnvironmentVariable("SSAS_DB_NAME");
+        private static string SSAS_REGION = Environment.GetEnvironmentVariable("SSAS_REGION");
+        private static string IDENTITY_CONN_STR = Environment.GetEnvironmentVariable("IDENTITY_CONN_STRING");
+        private static string SUBSCRIPTION_ID = Environment.GetEnvironmentVariable("SUBSCRIPTION_ID");
+        private static string RESOURCE_GROUP_NAME = Environment.GetEnvironmentVariable("RESOURCE_GROUP_NAME");
+        private static string FRONT_DOOR_NAME = Environment.GetEnvironmentVariable("FRONT_DOOR_NAME");
+        private static string BACKEND_POOL_NAME = Environment.GetEnvironmentVariable("BACKEND_POOL_NAME");
+        private static string BACKEND_ADDRESS = Environment.GetEnvironmentVariable("BACKEND_ADDRESS");
 
         [FunctionName("location")]
         public static HttpResponseMessage GetLocation(
@@ -30,74 +39,11 @@ namespace AsAzureFunctions {
         ) {
             
             log.LogInformation("GetLocation method has been triggered...");
-
-            var server = Environment.GetEnvironmentVariable("SSAS_SERVER_NAME");
+            
+            var server = $"asazure://{SSAS_REGION}.asazure.windows.net/{SSAS_DB_NAME}";
 
             var response = new HttpResponseMessage(HttpStatusCode.OK) {
                 Content = new StringContent(server)
-            };
-            return response;
-        }
-        
-        /*
-        [FunctionName("health")]
-        public static async Task<HttpResponseMessage> GetHealthAsync(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "head", Route = "server/health")] HttpRequest req,
-            ExecutionContext context,
-            ILogger log
-        ) {
-            
-            log.LogInformation("GetHealthAsync method has been triggered...");
-
-            // Get the health check URL from the settings
-            var url = Environment.GetEnvironmentVariable("HEALTH_CHECK_URL");
-            var idConnStr = Environment.GetEnvironmentVariable("IDENTITY_CONN_STRING");
-
-            // Get a token to access the Azure Resource Health API using the managed identity
-            var tokenProvider = new AzureServiceTokenProvider(idConnStr);
-            var token = await tokenProvider.GetAccessTokenAsync("https://management.azure.com/");
-
-            // Use the Azure Resource Health API to get the status health for the server
-            Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            var json = await Client.GetStringAsync(url);
-            var result = JsonConvert.DeserializeObject<HealthResponse>(json);
-
-            // Check the result
-            var status = HttpStatusCode.ServiceUnavailable;
-            if (result.properties.availabilityState.ToLower() == "available") {
-                status = HttpStatusCode.OK;
-            }
-
-            // Set the status message
-            var message = $"{result.properties.availabilityState}: {result.properties.summary}";
-
-            // Return the result
-            var response = new HttpResponseMessage(status) {
-                Content = new StringContent(message)
-            };
-            return response;
-        }
-        */
-
-        [FunctionName("health")]
-        public static async Task<HttpResponseMessage> GetHealthAsync(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "head", Route = "server/health")] HttpRequest req,
-            ExecutionContext context,
-            ILogger log
-        ) {
-            
-            log.LogInformation("GetHealthAsync method has been triggered...");
-
-            var status = HttpStatusCode.OK;
-            var message = String.Empty;
-            if (File.Exists(FILENAME) == true) {
-                message = await File.ReadAllTextAsync(FILENAME);
-                status = HttpStatusCode.ServiceUnavailable;
-            }
-
-            // Return the result
-            var response = new HttpResponseMessage(status) {
-                Content = new StringContent(message)
             };
             return response;
         }
@@ -110,67 +56,133 @@ namespace AsAzureFunctions {
             
             log.LogInformation("CheckHealthTimerAsync method has been triggered...");
 
-            await CheckHealthAsync(log);
+            // Check the status of the Azure Resource
+            var status = await CheckHealthAsync(log);
+
+            // Handle the Firewall
+            await UpdateFrontDoorAsync(status.IsHealthy, log);
         }
 
-        [FunctionName("check")]
-        public static Task<HttpResponseMessage> CheckHealthHttpAsync(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "server/health/check")] HttpRequest req,
+        [FunctionName("health")]
+        public static async Task<HttpResponseMessage> CheckHealthHttpAsync(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "server/health")] HttpRequest req,
             ExecutionContext context,
             ILogger log
         ) {
             
             log.LogInformation("CheckHealthHttpAsync method has been triggered...");
 
-            return CheckHealthAsync(log);
+            // Check the status of the Azure Resource
+            var status = await CheckHealthAsync(log);
+
+            // Handle the Firewall
+            await UpdateFrontDoorAsync(status.IsHealthy, log);
+
+            // Return the result
+            return new HttpResponseMessage() {
+                StatusCode = status.IsHealthy ? HttpStatusCode.OK : HttpStatusCode.ServiceUnavailable,
+                Content = new StringContent(status.Message)
+            };
         }
 
-        private static async Task<HttpResponseMessage> CheckHealthAsync(
-            ILogger log
-        ) {
+        private static async Task<HealthStatus> CheckHealthAsync(ILogger log) {
             
             log.LogInformation("CheckHealthAsync method has been triggered...");
 
-            // Get the health check URL from the settings
-            var url = Environment.GetEnvironmentVariable("HEALTH_CHECK_URL");
-            var idConnStr = Environment.GetEnvironmentVariable("IDENTITY_CONN_STRING");
-
             // Get a token to access the Azure Resource Health API using the managed identity
-            var tokenProvider = new AzureServiceTokenProvider(idConnStr);
+            var tokenProvider = new AzureServiceTokenProvider(IDENTITY_CONN_STR);
             var token = await tokenProvider.GetAccessTokenAsync("https://management.azure.com/");
+            var ssasHealthCheckUrl = $"https://management.azure.com/subscriptions/{SUBSCRIPTION_ID}/resourceGroups/{RESOURCE_GROUP_NAME}/providers/Microsoft.AnalysisServices/servers/{SSAS_DB_NAME}/providers/Microsoft.ResourceHealth/availabilityStatuses/current?api-version=2017-07-01";
 
             // Use the Azure Resource Health API to get the status health for the server
-            Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            var json = await Client.GetStringAsync(url);
-            var result = JsonConvert.DeserializeObject<HealthResponse>(json);
-
-            // Check the result
-            var status = HttpStatusCode.ServiceUnavailable;
-            if (result.properties.availabilityState.ToLower() == "available") {
-                status = HttpStatusCode.OK;
-            }
-
-            // DEBUG
-            //result.properties.availabilityState = "Unavailable";
-
-            // Set the status message
-            var message = $"{result.properties.availabilityState}: {result.properties.summary}";
-
-            // Check the result and if there's an error, write a file to storage
-            if (result.properties.availabilityState.ToLower() != "available") {
-                using (var file = File.CreateText(FILENAME)) {
-                    await file.WriteLineAsync(message);
-                }
-            }
-            else if (File.Exists(FILENAME) == true) {
-                File.Delete(FILENAME);
-            }
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var json = await client.GetStringAsync(ssasHealthCheckUrl);
+            var result = JsonSerializer.Deserialize<HealthResponse>(json);
 
             // Return the result
-            var response = new HttpResponseMessage(status) {
-                Content = new StringContent(message)
+            return new HealthStatus {
+                Status = result.properties.availabilityState,
+                Message = result.properties.summary
             };
-            return response;
+        }
+
+        private static async Task UpdateFrontDoorAsync(bool isHealthy, ILogger log) {
+
+            log.LogInformation("UpdateFrontDoorAsync method has been triggered...");
+
+            // Get a token to access the Azure Resource Health API using the managed identity
+            var tokenProvider = new AzureServiceTokenProvider(IDENTITY_CONN_STR);
+            var token = await tokenProvider.GetAccessTokenAsync("https://management.azure.com/");
+            var credentials = new TokenCredentials(token);
+            
+            var mgmtClient = new FrontDoorManagementClient(credentials) {
+                SubscriptionId = SUBSCRIPTION_ID
+            };
+
+            // Use the Azure Management SDK to get the Front Door
+            var frontDoor = await mgmtClient.FrontDoors.GetAsync(RESOURCE_GROUP_NAME, FRONT_DOOR_NAME);
+
+            // Find the associated Backend
+            var backend = frontDoor.BackendPools
+                .Single(p => p.Name == BACKEND_POOL_NAME)
+                .Backends.Single(s => s.Address == BACKEND_ADDRESS);
+
+            // Set the status of the front door backend
+            var newStatus = isHealthy ? "Enabled" : "Disabled";
+            
+            // If the status has changed, then update the status
+            if (backend.EnabledState != newStatus) {
+                backend.EnabledState = newStatus;
+                var result = await mgmtClient.FrontDoors.CreateOrUpdateAsync(RESOURCE_GROUP_NAME, FRONT_DOOR_NAME, frontDoor);
+            }
+        }
+
+        private class HealthStatus {
+
+            public string Status { get; set; }
+
+            public string Message { get; set; }
+
+            public bool IsHealthy {
+                get {
+                    return this.Status.ToLower() == "available";
+                }
+            }
+        }
+            
+        private class HealthResponse {
+
+            public string id { get; set; }
+
+            public string name { get; set; }
+
+            public string type { get; set; }
+
+            public string location { get; set; }
+
+            public HealthResponseProperties properties { get; set; }
+        }
+
+        private class HealthResponseProperties {
+
+            public string availabilityState { get; set; }
+
+            public string title { get; set; }
+
+            public string summary { get; set; }
+
+            public string reasonType { get; set; }
+
+            public DateTime rootCauseAttributionTime { get; set; }
+
+            public DateTime occuredTime { get; set; }
+
+            public string reasonChronicity { get; set; }
+
+            public DateTime reportedTime { get; set; }
+
+            public DateTime resolutionETA { get; set; }
         }
     }
 }
--- AsAzureFunctions/Models.cs
+++ AsAzureFunctions/Models.cs
@@ -4,78 +4,45 @@ using System.Text;
 
 namespace AsAzureFunctions {
 
-/*
+    /*
 
-// Available
-{
-    "id": "/subscriptions/b9c770d1-cde9-4da3-ae40-95ce1a4fac0c/resourcegroups/cdw-analysisservices-20200401/providers/microsoft.analysisservices/servers/cdwanalysisservices20200401/providers/Microsoft.ResourceHealth/availabilityStatuses/current",
-    "name": "current",
-    "type": "Microsoft.ResourceHealth/AvailabilityStatuses",
-    "location": "westus2",
-    "properties": {
-        "availabilityState": "Available",
-        "title": "Available",
-        "summary": "This server is running normally. There aren't any known Azure platform problems affecting this Analysis Services server.",
-        "reasonType": "",
-        "occuredTime": "2020-04-20T13:11:38Z",
-        "reasonChronicity": "Transient",
-        "reportedTime": "2020-04-20T13:13:50.7932991Z"
-    }
-}
-
-
-// Unavailable
-{
-    "id": "/subscriptions/b9c770d1-cde9-4da3-ae40-95ce1a4fac0c/resourcegroups/cdw-analysisservices-20200401/providers/microsoft.analysisservices/servers/cdwanalysisservices20200401/providers/Microsoft.ResourceHealth/availabilityStatuses/current",
-    "name": "current",
-    "type": "Microsoft.ResourceHealth/AvailabilityStatuses",
-    "location": "westus2",
-    "properties": {
-        "availabilityState": "Unavailable",
-        "title": "",
-        "summary": "Server is paused. Resume the server to make it available. Server is paused. Resume the server to make it available.",
-        "reasonType": "Customer Initiated",
-        "rootCauseAttributionTime": "2020-04-20T13:15:55.375Z",
-        "occuredTime": "2020-04-20T13:15:55Z",
-        "reasonChronicity": "Transient",
-        "reportedTime": "2020-04-20T13:17:21.4382042Z",
-        "resolutionETA": "2020-04-20T13:35:55Z"
-    }
-}
-
-*/
-
-    internal class HealthResponse {
-
-        public string id { get; set; }
-
-        public string name { get; set; }
-
-        public string type { get; set; }
-
-        public string location { get; set; }
-
-        public Properties properties { get; set; }
+    // Available
+    {
+        "id": "/subscriptions/b9c770d1-cde9-4da3-ae40-95ce1a4fac0c/resourcegroups/cdw-analysisservices-20200401/providers/microsoft.analysisservices/servers/cdwanalysisservices20200401/providers/Microsoft.ResourceHealth/availabilityStatuses/current",
+        "name": "current",
+        "type": "Microsoft.ResourceHealth/AvailabilityStatuses",
+        "location": "westus2",
+        "properties": {
+            "availabilityState": "Available",
+            "title": "Available",
+            "summary": "This server is running normally. There aren't any known Azure platform problems affecting this Analysis Services server.",
+            "reasonType": "",
+            "occuredTime": "2020-04-20T13:11:38Z",
+            "reasonChronicity": "Transient",
+            "reportedTime": "2020-04-20T13:13:50.7932991Z"
+        }
     }
 
-    internal class Properties {
 
-        public string availabilityState { get; set; }
-
-        public string title { get; set; }
-
-        public string summary { get; set; }
-
-        public string reasonType { get; set; }
-
-        public DateTime rootCauseAttributionTime { get; set; }
-
-        public DateTime occuredTime { get; set; }
-
-        public string reasonChronicity { get; set; }
-
-        public DateTime reportedTime { get; set; }
-
-        public DateTime resolutionETA { get; set; }
+    // Unavailable
+    {
+        "id": "/subscriptions/b9c770d1-cde9-4da3-ae40-95ce1a4fac0c/resourcegroups/cdw-analysisservices-20200401/providers/microsoft.analysisservices/servers/cdwanalysisservices20200401/providers/Microsoft.ResourceHealth/availabilityStatuses/current",
+        "name": "current",
+        "type": "Microsoft.ResourceHealth/AvailabilityStatuses",
+        "location": "westus2",
+        "properties": {
+            "availabilityState": "Unavailable",
+            "title": "",
+            "summary": "Server is paused. Resume the server to make it available. Server is paused. Resume the server to make it available.",
+            "reasonType": "Customer Initiated",
+            "rootCauseAttributionTime": "2020-04-20T13:15:55.375Z",
+            "occuredTime": "2020-04-20T13:15:55Z",
+            "reasonChronicity": "Transient",
+            "reportedTime": "2020-04-20T13:17:21.4382042Z",
+            "resolutionETA": "2020-04-20T13:35:55Z"
+        }
     }
+
+    */
+
 }
